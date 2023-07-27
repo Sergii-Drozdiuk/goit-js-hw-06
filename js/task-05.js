d19c4fdef1313a9f326891ad19da045c0adb1f33
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-07-27 20:16:07 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,12 +1,12 @@
-let nameInput = document.querySelector('#name-input');
-let nameOutput = document.querySelector('#name-output');
-nameInput.addEventListener('input', handlerSearch);
+let input = document.querySelector('#name-input');
+let span = document.querySelector('#name-output');
+input.addEventListener('input', handlerSearch);
 
 function handlerSearch(evt) {
    if (evt.currentTarget.value.length > 0) {
-      nameOutput.textContent = evt.currentTarget.value;
-      return nameOutput.textContent;
+      span.textContent = evt.currentTarget.value;
+      return span.textContent;
    }
-   nameOutput.textContent = "Anonymous";
+   span.textContent = "Anonymous";
 }
 
