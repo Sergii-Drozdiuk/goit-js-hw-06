8c10b1f41e4d6b95e462c9498c4271c6b3edb9bf
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-07-28 23:50:43 +0300

    task-08

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,12 @@
 const validationInput = document.querySelector('#validation-input');
 
-function handlerInputLength() {
+function handlerCheckLength() {
 
-   const dataLength = Number(validationInput.getAttribute("data-length"));
+   const dataLength = Number(validationInput.dataset.length);
    const actualLength = validationInput.value.trim().length;
    
     validationInput.classList.toggle('valid', actualLength === dataLength);
     validationInput.classList.toggle('invalid', actualLength !== dataLength);
 }
 
-validationInput.addEventListener('blur', handlerInputLength);
+validationInput.addEventListener('blur', handlerCheckLength);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', handlerSubmit)
+
+function handlerSubmit(evt) {
+    evt.preventDefault();
+     const emailValue = loginForm.elements.email.value;
+     const passwordValue = loginForm.elements.password.value;
+
+    if (!emailValue || !passwordValue) {
+      alert('All fields must be filled out.');
+    } else {
+      const formData = {
+        email: emailValue,
+        password: passwordValue,
+      };
+      console.log(formData);
+      loginForm.reset()
+   }
+}
 
