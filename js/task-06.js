f022f78dccb596dbf365c17e2d1800a83f5fa4dc
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-07-28 20:20:16 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const validationInput = document.querySelector('#validation-input');
 function handlerInputLength() {
 
    const dataLength = validationInput.getAttribute("data-length");
-   const actualLength = validationInput.value.length;
+   const actualLength = validationInput.value.trim().length;
    
     validationInput.classList.toggle('valid', actualLength == dataLength);
     validationInput.classList.toggle('invalid', actualLength != dataLength);
