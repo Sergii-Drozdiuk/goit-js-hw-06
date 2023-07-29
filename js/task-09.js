35e6e14296c6c0490f7005b0e170641e40bbce81
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-07-29 11:59:04 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,13 @@
+const btn = document.querySelector('.change-color');
+const span = document.querySelector('.color');
+const body = document.querySelector('body');
+
+btn.addEventListener('click', getRandomHexColor)
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+ let hexColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+   .padStart(6, 0)}`;
+  span.textContent = hexColor;
+  body.style.backgroundColor = `${hexColor}`;
 }
